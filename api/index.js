27692cb27688dc4e7c460608055c4056ac5e57af
harsh-devjs <harsh.devjs@gmail.com>
2025-05-27 15:43:50 +0530

    Added Middlewares, Error-Handling & User Auth

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from './routes/auth.js'
 import usersRoute from './routes/user.js'
 import hotelsRoute from './routes/hotels.js'
 import roomsRoute from './routes/rooms.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -14,8 +15,8 @@ const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
         console.log('Connected to MongoDB.')
-    } catch (error) {
-        throw error
+    } catch (err) {
+        throw err
     }
 }
 
@@ -24,11 +25,19 @@ mongoose.connection.on("disconnected", () => {
 })
 
 // MIDDLEWARES
+app.use(cookieParser())
 app.use(express.json())
 app.use('/api/auth', authRoute)
 app.use('/api/users', usersRoute)
 app.use('/api/hotels', hotelsRoute)
 app.use('/api/rooms', roomsRoute)
+
+// err HANDLING
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || 500
+    return res.status(errorStatus).json(errorMessage)
+})
 
 app.listen(8800, () => {
     connect()
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,66 +1,20 @@
 import express from 'express'
-import Hotel from '../models/Hotel.js'
-
+import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from '../controllers/hotelController.js'
+import { verifyAdmin } from '../utils/verifyToken.js'
 const router = express.Router()
 // CREATE
-router.post('/', async (req, res) => {
-
-    const newHotel = new Hotel(req.body)
-
-    try {
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    } catch (error) {
-        res.status(500).json({ "error": error })
-        console.log(error)
-    }
-})
+router.post('/', verifyAdmin, createHotel)
 
 // UPDATE
-router.put('/:id', async (req, res) => {
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(
-            req.params.id,
-            { $set: req.body },
-            { new: true }
-        )
-        res.status(200).json(updatedHotel)
-    } catch (error) {
-        res.status(500).json({ "error": error })
-        console.log(error)
-    }
-})
+router.put('/:id', verifyAdmin, updateHotel)
 
 // DELETE
-router.delete('/:id', async (req, res) => {
-    try {
-        await Hotel.findByIdAndUpdate(req.params.id)
-        res.status(200).json("Hotel has been deleted successfully.")
-    } catch (error) {
-        res.status(500).json({ "error": error })
-        console.log(error)
-    }
-})
+router.delete('/:id', verifyAdmin, deleteHotel)
 
 // GET
-router.get('/:id', async (req, res) => {
-    try {
-        const hotel = await Hotel.findById(req.params.id)
-        res.status(200).json(hotel)
-    } catch (error) {
-        res.status(500).json({ "error": error })
-        console.log(error)
-    }
-})
+router.get('/:id', getHotel)
+
 // GET ALL
-router.get('/', async (req, res) => {
-    try {
-        const hotels = await Hotel.find()
-        res.status(200).json(hotels)
-    } catch (error) {
-        res.status(500).json({ "error": error })
-        console.log(error)
-    }
-})
+router.get('/', getAllHotels)
 
 export default router
